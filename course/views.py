20c1cb864cbe8de708f31a6bbfc20171e52a0d44
Simon Escano <escanosimonlyster@gmail.com>
2024-10-06 12:57:58 +0800

    Added course categorization

--- course/views.py
+++ course/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import CourseForm
 
-# Create your views here.
+@login_required
+def add_a_course(request):
+    if request.method == 'POST':
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            course = form.save(commit=False)
+            course.created_by = request.user
+            course.save()
+            return redirect('/')
+    else:
+        form = CourseForm()
+
+    return render(request, 'course/add_a_course.html', {'form': form})
