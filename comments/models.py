71d39a69d82d1e6f4bf7b73c88cb3d56c98d7270
Simon Escano <escanosimonlyster@gmail.com>
2024-10-06 17:12:24 +0800

    Added comments

--- comments/models.py
+++ comments/models.py
@@ -4,10 +4,12 @@ from notes.models import Note
 
 class Comment(models.Model):
     content = models.TextField()
-
-    note = models.ForeignKey(Note, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    note = models.ForeignKey(Note, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} commented '{self.content}' on {self.note.title}"
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
