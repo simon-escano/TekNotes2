4c66cc000c33ef8026b40ec385b0649a281e1428
Simon Escano <escanosimonlyster@gmail.com>
2024-10-17 03:37:11 +0800

    Made Profile progress

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', views.logout, name='logout'),
-    path('profile/<int:user_id>/', views.profile, name='profile'),
+    path('profile/<int:user_id>/notes/', views.profile, name='profile'),
+    path('profile/<int:user_id>/likes/', views.profile_likes, name='profile_likes'),
+    path('profile/<int:user_id>/courses/', views.profile_courses, name='profile_courses'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as log_out
 from django.db.models import OuterRef, Subquery
+
+from course.models import Course
 from .forms import RegisterForm
 from .models import Profile
 from notes.models import Note
@@ -29,14 +31,30 @@ def logout(request):
 
 def profile(request, user_id):
     profile = Profile.objects.get(user__id=user_id)
-    public_notes = Note.objects.filter(created_by=user_id, is_private=False)
-    latest_likes = Like.objects.filter(note=OuterRef('pk'), user=request.user).order_by('-created_at')
-    liked_notes = Note.objects.filter(likes__user=request.user, is_private=False).annotate(
+    notes = Note.objects.filter(created_by=user_id, is_private=False)
+    
+    return render(request, 'authentication/profile.html', {
+        'profile': profile,
+        'notes': notes,
+    })
+
+def profile_likes(request, user_id):
+    profile = Profile.objects.get(user__id=user_id)
+    latest_likes = Like.objects.filter(note=OuterRef('pk'), user__id=user_id).order_by('-created_at')
+    notes = Note.objects.filter(likes__user__id=user_id, is_private=False).annotate(
         like_created_at=Subquery(latest_likes.values('created_at')[:1])
     ).distinct().order_by('-like_created_at')
     
     return render(request, 'authentication/profile.html', {
         'profile': profile,
-        'notes': public_notes,
-        'liked_notes': liked_notes
+        'notes': notes
+    })
+
+def profile_courses(request, user_id):
+    profile = Profile.objects.get(user__id=user_id)
+    courses = Course.objects.filter(created_by__id=user_id).distinct()
+    
+    return render(request, 'authentication/profile.html', {
+        'profile': profile,
+        'courses': courses,
     })
